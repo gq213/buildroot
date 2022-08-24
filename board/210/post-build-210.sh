aadf515fd9973284e39be45fbee9cfd86864d44d
gq213 <gaoqiang1211@gmail.com>
2022-08-24 15:38:27 +0800

    add rtl8821cu driver.

--- board/210/post-build-210.sh
+++ board/210/post-build-210.sh
@@ -7,3 +7,7 @@ rm -rf $TARGET/etc/init.d/S02klogd
 rm -rf $TARGET/etc/init.d/S02sysctl
 rm -rf $TARGET/etc/init.d/S20urandom
 rm -rf $TARGET/etc/init.d/S40network
+
+mkdir -p $TARGET/lib/modules/
+cp $BUILD_DIR/../../package/210/rtl8821cu/8821cu.ko $TARGET/lib/modules/ -a
+cp $BUILD_DIR/../../package/210/rtl8821cu/rtl8821cu.sh $TARGET/usr/bin/ -a
---
+++ package/210/rtl8821cu/rtl8821cu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+insmod /lib/modules/8821cu.ko rtw_drv_log_level=3
